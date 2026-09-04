a639ada0f82d0624a0299ec30fdd821924cb30da
niyaz <cashap.niyaz20@gmail.com>
2026-09-04 16:10:25 +0300

    v0.4 добавлено первое окно Corevia на PySide6

--- corevia/__main__.py
+++ corevia/__main__.py
@@ -1,5 +1,11 @@
+from corevia.presentation.dekstop.app import (
+    run_desktop,
+)
+
 def main() -> None:
-    print("Corevia")
+    raise SystemExit(
+        run_desktop()
+    )
 
 if __name__ == "__main__":
     main()
