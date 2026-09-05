781255b4df357dd1a41d8ae3d3bed204880a3651
niyaz <cashap.niyaz20@gmail.com>
2026-09-05 16:32:00 +0300

    добавлены pytest, Ruff и первый автоматический тест

---
+++ tests/presentation/test_i18n.py
@@ -0,0 +1,12 @@
+from corevia.presentation.common.i18n import I18n
+
+def test_i18n_switches_language() -> None:
+    i18n = I18n()
+
+    assert i18n.language == "en"
+    assert i18n.tr("app.welcome") == "Welcome to Corevia"
+
+    i18n.set_language("ru")
+
+    assert i18n.language == "ru"
+    assert i18n.tr("app.welcome") == "Добро пожаловать в Corevia"
